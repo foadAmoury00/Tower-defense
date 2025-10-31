9ef8187cc47c95f6109126d08774c7b0ffbbfd25
Fouad Elamoury <foad.elamoury@5d-vr.com>
2025-10-31 18:31:29 +0200

    enemy attack

---
+++ Assets/Scripts/Player/Attack/AutoAttack.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class AutoAttack : MonoBehaviour
+{
+    
+ 
+    public GameObject projectilePrefab;
+    public float attackRange = 10f;
+    public float timeBetweenShots = 0.5f;
+
+    private float shootTimer;
+    private Transform currentTarget;
+
+    public float projectileSpeed = 10f;
+
+    void Update()
+    {
+        FindTarget();
+
+        shootTimer -= Time.deltaTime;
+        if (currentTarget != null && shootTimer <= 0)
+        {
+            ShootTarget();
+            shootTimer = timeBetweenShots;
+        }
+    }
+
+    void FindTarget()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, attackRange, LayerMask.GetMask("Enemy"));
+        if (hitColliders.Length > 0)
+        {
+            currentTarget = hitColliders[0].transform;
+        }
+        else
+        {
+            currentTarget = null;
+        }
+    }
+
+    void ShootTarget()
+    {
+
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+
+        Rigidbody projectRB = projectile.GetComponent<Rigidbody>();
+
+        projectile.GetComponent<Rigidbody>().linearVelocity = (currentTarget.position - transform.position).normalized * projectileSpeed;
+    }
+}
+
+    
+
